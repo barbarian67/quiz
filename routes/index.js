9b8f2590c5c158cbbc6c7e74a7b99c7ad9472088
Jaime Canillas <jaime.canillas@iesfernandoaguilar.es>
2015-07-23 23:37:58 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-router.get('/author', quizController.author);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-
+// Los créditos
+router.get('/author', quizController.author);
 module.exports = router;
