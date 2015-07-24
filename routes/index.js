0cd0e4fbefc25c3819313a203ad88ca0673e3208
Jaime Canillas <jaime.canillas@iesfernandoaguilar.es>
2015-07-24 18:27:26 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes',                      	quizController.index);
+router.get('/quizes/:quizId(\\d+)',        	quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/new',					quizController.new);
+router.post('/quizes/create',				quizController.create);
 // Los créditos
 router.get('/author', quizController.author);
 module.exports = router;
